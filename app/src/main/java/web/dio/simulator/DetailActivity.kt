49c239f318ddac978e8c6c50ebe0822d38b9078f
IuriBreno <brenoiuri@gmail.com>
2022-03-03 21:37:19 -0300

    DetailActivity

---
+++ app/src/main/java/web/dio/simulator/DetailActivity.kt
@@ -0,0 +1,16 @@
+package web.dio.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import web.dio.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
